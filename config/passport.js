312a92a250fd8fa3bace3725f86dbde55fae47d8
Usman <usman.mail11@gmail.com>
2014-06-16 10:33:41 +0300

    serializing and deserializing users using passport apis

--- config/passport.js
+++ config/passport.js
@@ -3,5 +3,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var User =  require('../app/models/user');
 
 modeule.exports = function(passprt) {
-	
+	passprt.serializeUser(function(user, done) {
+		done(null, user.id);
+	});
 };
