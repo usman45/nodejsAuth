c8f5ff67a5587451bd79b1270e1320e492053161
Usman <usman.mail11@gmail.com>
2014-06-16 11:29:41 +0300

    added route to post users signup information

--- app/routes.js
+++ app/routes.js
@@ -34,4 +34,11 @@ module.exports = function(app, passport) {
 		res.redirect('/');
 	}
 
+	// process the signup form
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
+
 };
