4c3549a62a56a1e57ef1a3591d2f310b44253608
Usman <usman.mail11@gmail.com>
2014-06-16 22:01:48 +0300

    controllers is provided with the callback functions now for the routes

---
+++ app/controllers/users.js
@@ -0,0 +1,15 @@
+exports.homePage = function(req, res) {
+	res.render('index.ejs');
+}
+
+exports.login = function(req, res) {
+	res.render('login.ejs', {
+		message : req.flash('loginMessage')
+	});
+}
+
+exports.signup = function(req, res) {
+	res.render('signup.ejs', {
+		message : req.flash('signupMessage')
+	});
+}
