3ebe400dba9a80ca115e6f735b427401c77b80bf
Usman <usman.mail11@gmail.com>
2014-06-16 13:36:22 +0300

    sigin route added the routing config

--- app/routes.js
+++ app/routes.js
@@ -41,4 +41,11 @@ module.exports = function(app, passport) {
 		failureFlash : true // allow flash messages
 	}));
 
+	// process the signup form
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect : '/profile', // redirect to the secure profile section
+		failureRedirect : '/login', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
+
 };
