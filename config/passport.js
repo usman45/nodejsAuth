f6bb07ea0080d11333b02bf76c109afba7f0cd62
Usman <usman.mail11@gmail.com>
2014-06-16 13:36:49 +0300

    local sigin strategy added for the users login in

--- config/passport.js
+++ config/passport.js
@@ -33,7 +33,7 @@ module.exports = function(passport) {
 					} else {
 						var newUser = new User();
 						newUser.local.email = email;
-						newUser.local.password = password;
+						newUser.local.password = newUser.generateHash(password);;
 
 						newUser.save(function(err) {
 							if(err)
@@ -46,4 +46,32 @@ module.exports = function(passport) {
 			});	
 		}
 	));
+
+	passport.use('local-login', new LocalStrategy(
+		{
+			usernameField : 'email',
+			passwordField : 'password',
+			passReqToCallback : true	
+		},
+		function(req, email, password, done) {
+			// find a user whose email is the same as the forms email
+		// we are checking to see if the user trying to login already exists
+        User.findOne({ 'local.email' :  email }, function(err, user) {
+            // if there are any errors, return the error before anything else
+            if (err)
+                return done(err);
+
+            // if no user is found, return the message
+            if (!user)
+                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+
+			// if the user is found but the password is wrong
+            if (!user.validPassword(password))
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+
+            // all is well, return successful user
+            return done(null, user);
+        });
+
+    }));		
 };
