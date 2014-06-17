f2c9434ef6a3f2d9b573f15de3e2c0d49638bc9b
Usman <usman.mail11@gmail.com>
2014-06-17 12:01:03 +0300

    unneeded variable

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var port = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash= require('connect-flash');
-var auth = require('./config/auth');
 
 var configDB = require('./config/database.js');
 
