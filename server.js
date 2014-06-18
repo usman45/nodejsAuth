540a14dd80d5e08555678fceb66ed2fddc252d3a
Usman <usman.mail11@gmail.com>
2014-06-18 10:01:22 +0300

    documenting the config file

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// set up ======================================================================
+// get all the tools we need
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
@@ -7,31 +9,35 @@ var flash= require('connect-flash');
 
 var configDB = require('./config/database.js');
 
-mongoose.connect(configDB.db);
+// configuration ===============================================================
+mongoose.connect(configDB.db); // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration
 
 app.configure(function() {
 
-	//setup or express application middleware
-	app.use(express.logger('dev'));
-	app.use(express.cookieParser());
-	app.use(express.bodyParser());
+	//setup or express application 
+	app.use(express.logger('dev')); // log every request to the console
+	app.use(express.cookieParser()); //read cookies (needed for auth)
+	app.use(express.bodyParser()); //get information from html forms
 
-	app.set('view engine', 'ejs');
+	app.set('view engine', 'ejs'); // set up ejs for templating
 
+	// required for passport
 	app.use(express.session({
 		secret: 'ilovescotchscotchyscotchscotch'
-	}));
+	})); // session secret
+
 	app.use(passport.initialize());
-	app.use(passport.session());
-	app.use(flash());
+	app.use(passport.session()); // persistent login session
+	app.use(flash()); //use connect-flash for flash messages stored in session
 });
 
 
 //routes
-require('./app/routes.js')(app, passport);
+require('./app/routes.js')(app, passport); // load our toutes and pass in our app and fully configired passport
 
+// launch ======================================================================
 app.listen(port);
 
 console.log('the app started on ' + port);
