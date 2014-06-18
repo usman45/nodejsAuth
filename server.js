fb02fd408295f2bdef6a3114866d6ae12cecc6f2
Usman <usman.mail11@gmail.com>
2014-06-18 12:44:14 +0300

    custom view's directory path configuration

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ app.configure(function() {
 	app.use(express.cookieParser()); //read cookies (needed for auth)
 	app.use(express.bodyParser()); //get information from html forms
 
+	//Set views path, template engine and default layout
+    app.set('views', 'app/views');
 	app.set('view engine', 'ejs'); // set up ejs for templating
 
 	// required for passport
